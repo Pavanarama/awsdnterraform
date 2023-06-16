fdeb8b49c13ed3d8f9e3bb52c1e8c4a9828e4baa
root <root@ip-172-20-1-136.ec2.internal>
2023-06-16 03:52:16 +0000

    backend s3

--- backends3.tf
+++ backends3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraformstate"
-    key    = "statefile"
+    bucket = "terraformnewst"
+    key    = "statefile/terraform.tfstate"
     region = "us-east-1"
   }
 }
--- subnet.tf
+++ subnet.tf
@@ -9,13 +9,13 @@ resource "aws_subnet" "mysubnet1" {
   }
 }
 
-# resource "aws_subnet" "mysubnet2" {
-#   vpc_id     = aws_vpc.myvpc.id
-#   cidr_block = "10.1.2.0/24"
-#    enable_resource_name_dns_a_record_on_launch = "true"
-#     map_public_ip_on_launch = "true"
+ resource "aws_subnet" "mysubnet2" {
+   vpc_id     = aws_vpc.myvpc.id
+   cidr_block = "10.1.2.0/24"
+    enable_resource_name_dns_a_record_on_launch = "true"
+     map_public_ip_on_launch = "true"
 
-#   tags = {
-#     Name = "PrivateSunebt1"
-#   }
-# }
+   tags = {
+     Name = "PrivateSunebt1"
+   }
+ }
