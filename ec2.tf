ddd808ae7f1d1757dbea1fb81464027f67b961e8
Ram <devops4batchs@gmail.com>
2023-06-22 09:10:46 +0530

    modifed

---
+++ ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "myec2" {
+  ami           = ami-053b0d53c279acc90
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.PublicSunebt1.id
+  tags = {
+    Name = "MyEC2"
+  }
+
+}
--- s3bucket.tf
+++ s3bucket.tf
@@ -3,12 +3,13 @@ resource "aws_s3_bucket" "Mybucket1" {
   depends_on = [
     aws_vpc.myvpc
   ]
-
 }
-resource "aws_s3_bucket" "Mybucket2" {
-  bucket = var.bucket2
-  depends_on = [
-    aws_s3_bucket.Mybucket1
-  ]
 
-}
+
+# resource "aws_s3_bucket" "Mybucket2" {
+#   bucket = var.bucket2
+#   depends_on = [
+#     aws_s3_bucket.Mybucket1
+#   ]
+
+# }
